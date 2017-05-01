4451f3e518d506ffb01399d17d890e702bbb35a8
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-05-01 02:10:07 -0300

    Agregada clase para Mapa.

---
+++ TP2 - Bianchini, Posadas, Urtubey/src/mapa/Coordenada.java
@@ -0,0 +1,39 @@
+package mapa;
+
+public class Coordenada {
+
+	private String _nombre;
+	private double _latitud;
+	private double _longitud;
+	
+	public Coordenada(String n, double lat, double lon){
+		_nombre = n;
+		_latitud = lat;
+		_longitud = lon;
+	}
+	
+	public String getNombre(){
+		return _nombre;
+	}
+	
+	public double getLatitud(){
+		return _latitud;
+	}
+	
+	public double getLongitud(){
+		return _longitud;
+	}
+	
+	@Override
+	public String toString(){
+		return "Coord: { Nombre: " + _nombre +"; Latitud: " + _latitud + "; Longitud: " + _longitud + " }";
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if ( !(o instanceof Coordenada) )
+			throw new IllegalArgumentException("El parametro ingresado no es una Coordenada");
+		return o.toString().equals(this.toString());
+	}
+	
+}
---
+++ TP2 - Bianchini, Posadas, Urtubey/src/mapa/Mapa.java
@@ -0,0 +1,47 @@
+package mapa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import grafo.GrafoPesadoMapa;
+
+public class Mapa {
+
+	private GrafoPesadoMapa _grafoMapa;
+	private ArrayList<Coordenada> _coordenadas;
+	
+	public Mapa(List<Coordenada> listaCoordenadas){
+
+		_grafoMapa = new GrafoPesadoMapa(listaCoordenadas.size());
+		for(int i=0; i<listaCoordenadas.size(); i++){
+			_coordenadas.add(listaCoordenadas.get(i));
+		}
+	
+	}
+	
+	public Iterable<Coordenada> getCoordenadas(){
+		return _coordenadas;
+	}
+	
+	public void agregarRelacion(Coordenada c1, Coordenada c2){
+		agregarRelacion(c1, c2, 0);
+	}
+	
+	public void agregarRelacion(Coordenada c1, Coordenada c2, int cantPeajes){
+		chequearCoordenada(c1, "agregar una relacion");
+		chequearCoordenada(c2, "agregar una relacion");
+		_grafoMapa.agregarArista(_coordenadas.indexOf(c1), _coordenadas.indexOf(c2), obtenerDistancia(c1, c2), cantPeajes);
+	}
+
+
+	//TODO: Calcular distancia en plano 2D
+	private int obtenerDistancia(Coordenada c1, Coordenada c2){
+		return 0;
+	}
+
+	private void chequearCoordenada(Coordenada c, String accion){
+		if( !_coordenadas.contains(c) )
+			throw new IllegalArgumentException("Se intento " + accion + " con una coordenada inexistente");
+	}
+	
+}
